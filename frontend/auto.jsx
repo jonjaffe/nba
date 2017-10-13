1808e3d234c157ea9ccc752183de6c888a046339
Jon Jaffe <jonjaffe5@gmail.com>
2017-10-13 05:05:54 -0700

    Changes made to fetch

--- frontend/auto.jsx
+++ frontend/auto.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import request from 'superagent';
 
 export default class AutoComplete extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       inputVal: '',
-      img: "https://www.logodesignlove.com/images/classic/nba-logo.jpg"
+      img: "https://www.logodesignlove.com/images/classic/nba-logo.jpg",
+      stats: null
     };
     this.selectPlayer = this.selectPlayer.bind(this);
     this.handleInput = this.handleInput.bind(this);
     this.getPlayerImage = this.getPlayerImage.bind(this);
+    this.resetPlayer = this.resetPlayer.bind(this);
+    this.getPlayerStats = this.getPlayerStats.bind(this);
   }
 
   getPlayerImage(name) {
@@ -17,6 +21,44 @@ export default class AutoComplete extends React.Component {
     let newPlayerId = this.props.players[name]
     let newImageEnd = ".png"
     return newImageBase + newPlayerId + newImageEnd
+  }
+
+  getPlayerStats(name) {
+    let statsURLBase = "http://stats.nba.com/stats/playerdashboardbyyearoveryear?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerID=";
+    let statsPlayerId = this.props.players[name];
+    let statsURLEnd = "&PlusMinus=N&Rank=N&Season=2016-17&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&Split=yoy&VsConference=&VsDivision=";
+    let statsURL = statsURLBase + statsPlayerId + statsURLEnd;
+    // request
+    //   .get(statsURL)
+    //   .withCredentials()
+    //   .then(function(err, res){
+    //     if (err) throw err;
+    //     console.log(res.text);
+    //   })
+    let myHeaders = new Headers();
+    const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+    const myInit = {method: 'GET', mode: 'no-cors', headers: myHeaders, cache: 'default'};
+    fetch(endpoint, myInit)
+  .then(response => {
+    return response.blob();
+    // console.log(response.json())
+  })
+  // {
+  //   console.log(statsURL);
+  //   return response.json()
+  // })
+  .then(data => console.log(data))
+  // ;function(data) {
+  //   console.log(data.resultSets.rowSet)
+  // })
+  .catch(err => console.log(err))
+  // ;function(err) {
+  //   console.error(err)
+  // })
+  }
+
+  resetPlayer() {
+    this.setState({inputVal: '', img: 'https://www.logodesignlove.com/images/classic/nba-logo.jpg'})
   }
 
   handleInput(event) {
@@ -45,7 +87,7 @@ export default class AutoComplete extends React.Component {
 
   selectPlayer(event) {
     let player = event.currentTarget.innerText;
-    this.setState({inputVal: player, img: this.getPlayerImage(player)});
+    this.setState({inputVal: player, img: this.getPlayerImage(player), stats: this.getPlayerStats(player)});
   }
 
   render() {
@@ -62,11 +104,13 @@ export default class AutoComplete extends React.Component {
             onChange={this.handleInput}
             value={this.state.inputVal}
             placeholder='Player Name'/>
+          <input type='button' onClick={this.resetPlayer} />
           <ul>
               {results}
           </ul>
         </div>
         <img src={this.state.img} />
+        {this.state.stats}
       </div>
     );
   }
